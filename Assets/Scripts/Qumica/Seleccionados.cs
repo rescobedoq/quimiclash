d8ee3dc39e6dc0b8553e03c436ef2337c9ce7109
Leonardo Ruben Arce Mayhua <larcem@gmail.com>
2025-12-07 18:47:23 -0500

    Añadir cantidad visible

--- Assets/Scripts/Qumica/Seleccionados.cs
+++ Assets/Scripts/Qumica/Seleccionados.cs
@@ -200,6 +200,14 @@ public class Seleccionados : MonoBehaviour
                 Debug.Log("3. Plantilla encontrada: " + plantilla.name);
                 imagenDescripcionUI.sprite = plantilla.imagenDescripcion;
                 imagenDescripcionUI.color = Color.white;
+
+                // AÑADIDO: Actualizar la cantidad del objeto mostrado
+                MostradorCantidad mostrador = FindFirstObjectByType<MostradorCantidad>();
+                if (mostrador != null)
+                {
+                    mostrador.MostrarCantidadDePlantilla(plantilla);
+                    Debug.Log("4. Cantidad actualizada: " + plantilla.cantidad);
+                }
             }
             else
             {
